9265bfe27487610c50d3271d8380aa8c289d3e64
Hayden Rinn <haydenrinn@gmail.com>
2020-12-03 00:10:04 -0800

    New icon poggers

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,10 +5,9 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.paint.Color;
 import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-import javafx.scene.control.Button;
 
 import main.events.Event;
 import main.events.EventManager;
@@ -16,7 +15,6 @@ import model.Grid;
 import simulation.EnergySimulator;
 import visualization.GridScene;
 
-import java.awt.*;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -24,9 +22,10 @@ import java.util.List;
 public class Main extends Application {
 
     private static final String TITLE = "GridFlow";
-    private static final String WINDOWICONPATH = "/resources/iconScaled.png";
+    private static final String WINDOW_ICON_PATH = "resources/iconLarge.png";
     private static final int WINDOW_WIDTH = 1300;
     private static final int WINDOW_HEIGHT = 700;
+    private static final String GRIDFLOW_BLUE = "#008EB0";
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -62,27 +61,15 @@ public class Main extends Application {
         root.getChildren().addAll(moduleGuiRoots);
         root.getChildren().add(mainGui);
         Scene scene = new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT);
-        //scene.setFill(Color.LIGHTGRAY);
+        scene.setFill(Color.LIGHTGRAY);
 
         primaryStage.setTitle(TITLE);
-        primaryStage.getIcons().add(new Image(WINDOWICONPATH));
+        primaryStage.getIcons().add(new Image(WINDOW_ICON_PATH));
         primaryStage.setMinHeight(700);
         primaryStage.setMinWidth(500);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
-
-/*    private Button CreateRoundButton(){
-        Button roundButton = new Button("Account");
-
-        roundButton.setStyle(
-
-
-        );
-
-        return roundButton;
-    }*/
-
 
     public static void main(String[] args) {
         launch(args);
